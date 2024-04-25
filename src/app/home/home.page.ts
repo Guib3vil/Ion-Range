44660891147ac71c2c6e320c47d856b6fac8dc07
GuiBaioni <guilhermebaioni35@gmail.com>
2024-04-25 15:49:00 -0300

    Ion-Range

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RangeCustomEvent } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+valorRange=0;
 
+onIonChange(ev:RangeCustomEvent) {
+  this.valorRange =parseInt(ev.detail.value.toString());
+}
 }
